e79c772e5569ae8b9e22a583c86ec7082c82a31c
Fireblade2534 <fireblade5234@gmail.com>
2026-02-25 12:03:29 -0500

    Implemented statemachine for the shooter

--- src/main/java/frc/robot/subsystems/turret/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/turret/ShooterSubsystem.java
@@ -21,11 +21,21 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.libraries.SubsystemStateMachine;
 
-public class ShooterSubsystem extends SubsystemBase {
+public class ShooterSubsystem extends SubsystemStateMachine<frc.robot.subsystems.turret.ShooterSubsystem.ShooterState> {
+    private final double SHOOTER_THRESHOLD = 1; // RPS
+
+    public enum ShooterState {
+        IDLE,
+        SPOOLING,
+        READY
+    }
+
     private SparkMaxConfig shooterConfig1;
     private SparkMaxConfig shooterConfig2;
     private SparkMax shooterMotor1;
@@ -53,6 +63,8 @@ public class ShooterSubsystem extends SubsystemBase {
     private double shooterPrevSetpointVelocity = 0;
 
     public ShooterSubsystem() {
+        super(ShooterState.IDLE);
+
         if (Constants.ShooterConstants.ENABLED) { 
             shooterMotor1 = new SparkMax(Constants.ShooterConstants.SHOOTER_MOTOR_1_ID, MotorType.kBrushless);
             shooterMotor2 = new SparkMax(Constants.ShooterConstants.SHOOTER_MOTOR_2_ID, MotorType.kBrushless);
@@ -87,7 +99,7 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public AngularVelocity getSpeed() {
-        if (Constants.ShooterConstants.ENABLED == false) {
+        if (shooterEncoder1 == null || shooterEncoder2 == null) {
             return RotationsPerSecond.of(0);
         } 
         
@@ -103,29 +115,75 @@ public class ShooterSubsystem extends SubsystemBase {
         shooterPrevSetpointVelocity = currentSpeed;
     }
 
+    private double calculateShooterVoltage() {
+        double shooterVoltage = shooterPID.calculate(getSpeed().in(RotationsPerSecond));
+        TrapezoidProfile.State shooterState = shooterPID.getSetpoint();
+        shooterVoltage += shooterFF.calculateWithVelocities(
+            shooterPrevSetpointVelocity,
+            shooterState.position
+        );
+        shooterVoltage = MathUtil.clamp(
+            shooterVoltage, 
+            -10.0, 
+        10.0
+        );
+
+        shooterPrevSetpointVelocity = shooterState.position;
+
+        return shooterVoltage;
+    }
+
     @Override
     public void periodic() {
-        if (Constants.ShooterConstants.ENABLED) {
-            double shooterVoltage = shooterPID.calculate(getSpeed().in(RotationsPerSecond));
-            TrapezoidProfile.State shooterState = shooterPID.getSetpoint();
-            shooterVoltage += shooterFF.calculateWithVelocities(
-                shooterPrevSetpointVelocity,
-                shooterState.position
-            );
-            shooterVoltage = MathUtil.clamp(
-                shooterVoltage, 
-                -10.0, 
-            10.0
-            );
-            shooterMotor1.setVoltage(shooterVoltage);
-            shooterMotor2.setVoltage(shooterVoltage);
+        
+        if (Constants.ShooterConstants.ENABLED == false) {
+            return;
+        }
+        // Safety Check as the desired state should only ever IDLE or READY
+        if (getDesiredState() == ShooterState.SPOOLING) {
+            setDesiredState(ShooterState.IDLE);
+        }
+        
+        double shooterVoltage = 0.0;
+        switch (getCurrentState()) {
+            case IDLE:
+                shooterVoltage = 0.0;
 
-            shooterPrevSetpointVelocity = shooterState.position;
+                if (getDesiredState() == ShooterState.READY) {
+                    resetShooter();
+                    transitionTo(ShooterState.SPOOLING);
+                }
+                break;
+            case SPOOLING:
+                shooterVoltage = calculateShooterVoltage();
 
-            SmartDashboard.putNumber("Shooter/Motor Voltage", shooterVoltage);
+                if (getDesiredState() == ShooterState.IDLE) {
+                    transitionTo(ShooterState.IDLE);
+                } else if (Math.abs(getSpeed().in(RotationsPerSecond) - getTargetSpeed().in(RotationsPerSecond)) < SHOOTER_THRESHOLD) {
+                    transitionTo(ShooterState.READY);
+                }
 
-            SmartDashboard.putNumber("Shooter/Motor 1 Speed", shooterEncoder1.getVelocity() * 60.0);
-            SmartDashboard.putNumber("Shooter/Motor 2 Speed", shooterEncoder2.getVelocity() * 60.0);
-        }  
+                break;
+            case READY:
+                shooterVoltage = calculateShooterVoltage();
+
+                if (getDesiredState() == ShooterState.IDLE) {
+                    transitionTo(ShooterState.IDLE);
+                } else if (Math.abs(getSpeed().in(RotationsPerSecond) - getTargetSpeed().in(RotationsPerSecond)) > SHOOTER_THRESHOLD) {
+                    transitionTo(ShooterState.SPOOLING);
+                }
+
+                break;
+        }
+        shooterMotor1.setVoltage(shooterVoltage);
+        shooterMotor2.setVoltage(shooterVoltage);
+
+        SmartDashboard.putNumber("Shooter/Motor Voltage", shooterVoltage);
+
+        SmartDashboard.putNumber("Shooter/Motor 1 Speed", shooterEncoder1.getVelocity() * 60.0);
+        SmartDashboard.putNumber("Shooter/Motor 2 Speed", shooterEncoder2.getVelocity() * 60.0);
+
+        SmartDashboard.putString("Shooter/Current State", getCurrentState().name());
+        SmartDashboard.putString("Shooter/Desired State", getDesiredState().name());
     }
 }
